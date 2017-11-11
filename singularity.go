3b10f835443d8fba1ece6033267286c921daff1b
Lenfree Yeung <lenfree.yeung@gmail.com>
2017-11-11 14:58:40 +1100

    add endpoint for http url

--- singularity.go
+++ singularity.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"time"
 
 	retryablehttp "github.com/hashicorp/go-retryablehttp"
@@ -13,11 +14,12 @@ import (
 
 // Client contains Singularity endpoint for http requests
 type Client struct {
-	Host                string
-	Port                int
+	Endpoint            string
 	RetryableHTTPClient retryablehttp.Client
 }
 
+// Config contains Singularity HTTP endpoint and configuration for
+// retryablehttp client's retry options
 type Config struct {
 	Host         string
 	Port         int
@@ -33,16 +35,23 @@ func New(c Config) *Client {
 	a.RetryWaitMax = time.Duration(c.RetryWaitMax)
 	a.RetryMax = c.RetryMax
 	return &Client{
-		Host:                c.Host,
-		Port:                c.Port,
+		Endpoint:            endpoint(&c),
 		RetryableHTTPClient: *a,
 	}
+}
+
+func endpoint(c *Config) string {
+	// if port is uninitialised, port would be http/80.
+	if c.Port == 0 {
+		return "http://" + c.Host
+	}
+	return "http://" + c.Host + ":" + strconv.Itoa(c.Port)
 }
 
 // GetRequests returns all Singularity requests.
 // https://github.com/HubSpot/Singularity/blob/master/Docs/reference/api.md#endpoint-/api/requests
 func (c *Client) GetRequests() (Requests, error) {
-	r, err := c.RetryableHTTPClient.Get("http://" + c.Host + "/api/requests")
+	r, err := c.RetryableHTTPClient.Get(c.Endpoint + "/api/requests")
 	if err != nil {
 		return nil, fmt.Errorf("Get requests error: %v", err)
 	}
